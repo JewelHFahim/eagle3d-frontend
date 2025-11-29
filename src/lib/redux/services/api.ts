d9769b5434093cbc494e9910d7e22187fdb2dd47
JewelHFahim <jewelhfahim@gmail.com>
2025-11-30 03:24:13 +0600

    fix delete issues

--- src/lib/redux/services/api.ts
+++ src/lib/redux/services/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = "https://backend-delta-eight-70.vercel.app";
 
 export const api = createApi({
   reducerPath: "api",
